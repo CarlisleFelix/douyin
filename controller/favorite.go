7f05564e25138b72a4b39d87eb2fa8ac0affdca4
1251776527 <1251776527@qq.com>
2023-08-17 18:15:49 +0800

    点赞操作，获取喜欢列表部分

--- controller/favorite.go
+++ controller/favorite.go
@@ -1,15 +1,71 @@
 package controller
 
 import (
+	"douyin/response"
+	"douyin/service"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
 func FavoriteAction(c *gin.Context) {
+	// 解析token生成user_id, 获取video_id, action_type
+	user_id, _ := c.Get("userid")
+	video_id := c.Query("video_id")
+	action_type := c.Query("action_type")
 
-	return
+	// 初始化返回值
+	var StatusCode int32
+	var StatusMsg string
+
+	// 参数校验
+	// action_type 不为1或2时
+	actionType, err := strconv.Atoi(action_type)
+	if err != nil {
+		StatusCode = 1
+		StatusMsg = "未知操作"
+		c.JSON(http.StatusOK, response.Favorite_Action_Response{
+			Response: response.Response{StatusCode: StatusCode, StatusMsg: StatusMsg},
+		})
+	} else if actionType != 1 && actionType != 2 {
+		StatusCode = 1
+		StatusMsg = "未知操作"
+		c.JSON(http.StatusOK, response.Favorite_Action_Response{
+			Response: response.Response{StatusCode: StatusCode, StatusMsg: StatusMsg},
+		})
+	}
+
+	// 处理请求
+	StatusCode, StatusMsg = service.FavoriteAction(user_id.(int64), video_id, int32(actionType))
+
+	// 返回
+	c.JSON(http.StatusOK, response.Favorite_Action_Response{
+		Response: response.Response{StatusCode: StatusCode, StatusMsg: StatusMsg},
+	})
 }
 
 func FavoriteList(c *gin.Context) {
+	// 获取参数
+	user_id := c.Query("user_id")
+	userId, _ := c.Get("userid")
 
-	return
+	// 参数校验
+	// 判断token解析出的userid与user_id是否一致
+	id, _ := strconv.ParseInt(user_id, 10, 64)
+	if id != userId {
+		c.JSON(http.StatusOK, response.Favorite_List_Response{
+			Response:  response.Response{StatusCode: 1, StatusMsg: "token与user_id不一致"},
+			VideoList: nil,
+		})
+		return
+	}
+
+	// 处理请求
+	statusCode, statusMsg, video_list := service.FavoriteList(id)
+
+	// 返回
+	c.JSON(http.StatusOK, response.Favorite_List_Response{
+		Response:  response.Response{StatusCode: statusCode, StatusMsg: statusMsg},
+		VideoList: video_list,
+	})
 }
--- service/favoriteService.go
+++ service/favoriteService.go
@@ -1,1 +1,231 @@
 package service
+
+import (
+	"douyin/dao"
+	"douyin/global"
+	"douyin/model"
+	"douyin/response"
+	"strconv"
+)
+
+func FavoriteAction(user_id int64, video_id string, action_type int32) (StatusCode int32, StatusMsg string) {
+	// 参数类型转换
+	videoId, err := strconv.ParseInt(video_id, 10, 64)
+	if err != nil {
+		StatusCode = 1
+		StatusMsg = "视频id异常"
+		return
+	}
+
+	// 开始执行
+	tx := global.SERVER_DB.Begin()
+
+	// 获取点赞人信息
+	var user model.User
+	user, StatusCode, StatusMsg = dao.SearchUser(user_id)
+	if StatusCode == 1 {
+		tx.Rollback()
+		return
+	}
+
+	// 获取视频信息
+	var video model.Video
+	video, StatusCode, StatusMsg = dao.SearchVideo(videoId)
+	if StatusCode == 1 {
+		tx.Rollback()
+		return
+	}
+
+	// 获取作者信息
+	var author model.User
+	author, StatusCode, StatusMsg = dao.SearchUser(video.Author_id)
+	if StatusCode == 1 {
+		tx.Rollback()
+		return
+	}
+
+	// 当用户对自己的视频点赞时，操作失败
+	if video.Author_id == user_id {
+		StatusCode = 1
+		StatusMsg = "该视频为本人发布，无法进行点赞或取消点赞操作"
+		tx.Rollback()
+		return
+	}
+
+	// 根据user_id,video_id查找点赞信息
+	favorite, result := dao.SearchFavorite(user_id, videoId)
+
+	// 对action_type进行判断, 1-点赞， 2-取消点赞
+	if action_type == 1 {
+		// 如果是点赞操作
+		// 先查询是否已经点赞
+		if result.RowsAffected == 0 {
+			// 如果未点赞，则将点赞信息存储到表中
+			// 视频点赞数+1
+			result = dao.UpdateVideo(video, action_type)
+			if result.Error != nil {
+				StatusCode = 1
+				StatusMsg = "视频点赞更新异常"
+				tx.Rollback()
+				return
+			}
+			// 用户点赞数+1
+			result = dao.UpdateUser(user, action_type)
+			if result.Error != nil {
+				StatusCode = 1
+				StatusMsg = "用户点赞更新异常"
+				tx.Rollback()
+				return
+			}
+			// 作者获赞数+1
+			result = dao.UpdateAuthor(author, action_type)
+			if result.Error != nil {
+				StatusCode = 1
+				StatusMsg = "作者获赞更新异常"
+				tx.Rollback()
+				return
+			}
+
+			// 创建点赞记录
+			StatusCode, StatusMsg = dao.CreateFavorite(user_id, videoId)
+			// 根据状态码判断点赞操作是否成功
+			if StatusCode == 1 {
+				tx.Rollback()
+			} else {
+				tx.Commit()
+			}
+			return
+		} else {
+			// 如果已点赞
+			StatusCode = 1
+			StatusMsg = "该视频已点赞"
+			tx.Rollback()
+			return
+		}
+	} else {
+		// 如果是取消点赞操作，先查询表中是否存在点赞记录，
+		if result.RowsAffected == 0 {
+			// 如果不存在，则返回异常
+			StatusCode = 1
+			StatusMsg = "未对该视频点赞，无法取消点赞"
+			tx.Rollback()
+			return
+		} else {
+			// 如果存在，则删除该记录
+			// 视频点赞数-1
+			result = dao.UpdateVideo(video, action_type)
+			if result.Error != nil {
+				StatusCode = 1
+				StatusMsg = "视频点赞更新异常"
+				tx.Rollback()
+				return
+			}
+			// 用户点赞数-1
+			result = dao.UpdateUser(user, action_type)
+			if result.Error != nil {
+				StatusCode = 1
+				StatusMsg = "用户点赞更新异常"
+				tx.Rollback()
+				return
+			}
+			// 作者获赞数-1
+			result = dao.UpdateAuthor(author, action_type)
+			if result.Error != nil {
+				StatusCode = 1
+				StatusMsg = "作者获赞更新异常"
+				tx.Rollback()
+				return
+			}
+
+			// 删除点赞记录
+			StatusCode, StatusMsg = dao.DeleteFavorite(favorite)
+			// 根据状态码判断操作是否成功
+			if StatusCode == 1 {
+				tx.Rollback()
+			} else {
+				tx.Commit()
+			}
+			return
+		}
+	}
+}
+
+func FavoriteList(user_id int64) (StatusCode int32, StatusMsg string, videoList []response.Video_Response) {
+	tx := global.SERVER_DB.Begin()
+	favorites, result := dao.SearchFavoriteList(user_id)
+	if result.Error != nil {
+		StatusCode = 1
+		StatusMsg = "获取用户喜欢列表异常"
+		tx.Rollback()
+		return StatusCode, StatusMsg, nil
+	} else if result.RowsAffected == 0 {
+		StatusCode = 0
+		StatusMsg = "用户无喜欢视频"
+		tx.Commit()
+		return StatusCode, StatusMsg, nil
+	}
+
+	// 创建视频列表
+	videoList = make([]response.Video_Response, len(favorites))
+
+	//
+	for i := 0; i < len(favorites); i++ {
+		// 获取视频信息
+		var video model.Video
+		video, StatusCode, StatusMsg = dao.SearchVideo(favorites[i].Video_id)
+		if StatusCode == 1 {
+			tx.Rollback()
+			return StatusCode, StatusMsg, nil
+		}
+		// 获取用户信息
+		var user model.User
+		user, StatusCode, StatusMsg = dao.SearchUser(video.Author_id)
+		if StatusCode == 1 {
+			tx.Rollback()
+			return StatusCode, StatusMsg, nil
+		}
+		// 获取关注信息
+		result = dao.SearchRelation(user_id, user.User_id)
+		if result.Error != nil {
+			StatusCode = 1
+			StatusMsg = "获取关注信息异常"
+			tx.Rollback()
+			return StatusCode, StatusMsg, nil
+		}
+
+		var IsFollow bool
+		// 设置关注信息
+		if result.RowsAffected == 0 {
+			IsFollow = false
+		} else {
+			IsFollow = true
+		}
+
+		videoList[i] = response.Video_Response{
+			Id: video.Video_id,
+			// 存储作者信息
+			Author: response.User_Response{
+				Id:              user.User_id,
+				Name:            user.User_name,
+				FollowCount:     user.Follow_count,
+				FollowerCount:   user.Follower_count,
+				IsFollow:        IsFollow,
+				Avatar:          user.Avatar,
+				BackgroundImage: user.Background_image,
+				Signature:       user.Signature,
+				TotalFavorited:  user.Total_favorited,
+				WorkCount:       user.Work_count,
+				FavoriteCount:   user.Favorite_count,
+			},
+			PlayUrl:       video.Play_url,
+			CoverUrl:      video.Cover_url,
+			FavoriteCount: video.Favorite_count,
+			CommentCount:  video.Comment_count,
+			IsFavorite:    true,
+			Title:         video.Title,
+		}
+	}
+
+	tx.Commit()
+	return StatusCode, StatusMsg, videoList
+}
